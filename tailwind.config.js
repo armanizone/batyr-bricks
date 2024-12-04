66da60091745657f582fe32e01156d38c06a8a94
armanizone <kurama.zxc@mail.ru>
2024-12-04 17:19:19 +0500

    w

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,22 @@
 /** @type {import('tailwindcss').Config} */
+
+import theme from 'tailwindcss/defaultTheme'
+
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      container: {
+        padding: '12px',
+        center: true,
+        screens: {
+          '2xl': theme.screens.lg
+        }
+      }
+    },
   },
   plugins: [],
 }
-
